121729b3a1b4d72be516ca59e3892888b10c59e1
Aeoniixx <aeoniixx@gmail.com>
2025-03-15 20:41:56 +0000

    msm8998-common: Update boot-image-profile.txt path for A15 QPR2
    * frameworks/base/config/boot-image-profile.txt now moved to frameworks/base/boot/boot-image-profile.txt
    
    Change-Id: I61af081b402f6e63722a5ae9e2215122

--- msm8998.mk
+++ msm8998.mk
@@ -461,7 +461,7 @@ PRODUCT_COPY_FILES += \
 
 # Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/msm8998-common/msm8998-common-vendor.mk)
